b60991cdf7f167ea4376a0c28d9420513d25fab3
gap-sys <suporteamerica@hotmail.com>
2025-06-23 01:02:45 -0300

    UPDATE

---
+++ src/presentation/assets/images/index.ts
@@ -0,0 +1,31 @@
+import LOGO_HOT_INVEST from './LOGO_HOT_INVEST.jpg'
+import CARTAO from './CARTAO.webp'
+import EQUIPE_REUNIDA from './EQUIPE_REUNIDA.webp'
+import CELULAR_CARTAO from './CELULAR_CARTAO.webp'
+import CONFIANCA from './CONFIANCA.jpg'
+import DESTINO from './DESTINO.jpg'
+import FE from './FE.jpg'
+import INOVACAO from './INOVACAO.jpg'
+import MERITOCRACIA from './MERITOCRACIA.jpg'
+import SIMPLICIDADE from './SIMPLICIDADE.jpg'
+import VISAO from './VISAO.jpg'
+import MISSAO from './MISSAO.jpg'
+import HEADER_IMAGE from './HEADER_IMAGE.jpg'
+
+const IMAGE = {
+  HEADER_IMAGE,
+  LOGO_HOT_INVEST,
+  CARTAO,
+  EQUIPE_REUNIDA,
+  CELULAR_CARTAO,
+  CONFIANCA,
+  DESTINO,
+  FE,
+  INOVACAO,
+  MERITOCRACIA,
+  SIMPLICIDADE,
+  VISAO,
+  MISSAO,
+}
+
+export default IMAGE
